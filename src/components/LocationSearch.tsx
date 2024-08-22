8851d24b1e4e779b26545833f013f224544f2484
Shiwanshu Shubham <shiwanshudev@protonmail.com>
2024-08-22 23:16:52 +0530

    Add location search functionality

--- src/components/LocationSearch.tsx
+++ src/components/LocationSearch.tsx
@@ -1,3 +1,50 @@
-export default function LocationSearch() {
-  return <div>Search for a location!</div>;
+import type { Place } from "../api/Place";
+import { Fragment, useState } from "react";
+import { search } from "../api/search";
+
+interface LocationSearchProps {
+  onPlaceClick: (place: Place) => void;
+}
+
+export default function LocationSearch({ onPlaceClick }: LocationSearchProps) {
+  const [places, setPlaces] = useState<Place[]>([]);
+  const [term, setTerm] = useState("");
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const results = await search(term);
+    setPlaces(results);
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="term" className=" font-bold">
+          Search
+        </label>
+        <input
+          type="text"
+          className="border border-gray-300 rounded-md shadow-sm focus:border-indigo-500 px-4 py-2 w-full"
+          id="term"
+          value={term}
+          onChange={(e) => setTerm(e.target.value)}
+        />
+      </form>
+      <h1 className="font-bold mt-6">Found Location</h1>
+      <div className="grid grid-cols-[1fr_40px] gap-2 mt-2 items-center">
+        {places.map((place) => {
+          return (
+            <Fragment key={place.id}>
+              <p className="text-sm">{place.name}</p>
+              <button
+                className="bg-blue-500 text-xs text-white font-bold py-1 px-1 rounded"
+                onClick={() => onPlaceClick(place)}
+              >
+                Go
+              </button>
+              <div className="border-b w-full col-span-2"></div>
+            </Fragment>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,3 +1,9 @@
-export default function Map() {
+import type { Place } from "../api/Place";
+
+interface MapProps {
+  place: Place | null;
+}
+
+export default function Map({ place }: MapProps) {
   return <div>Map!!</div>;
 }
