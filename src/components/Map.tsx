276c31af8556ca2f53b73892aa91bb07db7d2244
ss <Shiwanshu Shubham>
2024-08-23 18:57:51 +0530

    Add leaflet packages and map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,9 +1,31 @@
 import type { Place } from "../api/Place";
+import "leaflet/dist/leaflet.css";
+import type { Map as LeafletMap } from "leaflet"; // Displays maps
+import { useEffect, useRef } from "react";
+import { TileLayer, MapContainer, Marker } from "react-leaflet"; // Compatibility layer between leaflet and react
 
 interface MapProps {
   place: Place | null;
 }
 
 export default function Map({ place }: MapProps) {
-  return <div>Map!!</div>;
+  const mapRef = useRef<LeafletMap | null>(null); // Generic
+  useEffect(() => {
+    if (mapRef.current && place) {
+      mapRef.current.flyTo([place.latitude, place.longitude]);
+    }
+  }, [place]);
+
+  return (
+    <MapContainer
+      ref={mapRef}
+      center={[40.7, -74]}
+      zoom={12}
+      scrollWheelZoom
+      className="h-full"
+    >
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      {place && <Marker position={[place.latitude, place.longitude]} />}
+    </MapContainer>
+  );
 }
